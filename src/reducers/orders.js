6406bd305a032dc5674185245d9c008b87642a14
svetultus <svetultus@gmail.com>
2019-08-12 17:35:55 +0300

    main functions

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -8,10 +8,49 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
 export default (state = [], action) => {
+  const newState = state.slice();
+  let currentPosition, newPosition;
+
   switch (action.type) {
+    case 'CREATE_NEW_ORDER':
+      newState.push(
+        Object.assign({}, action.payload, {
+          ingredients: [],
+          position: 'clients'
+        })
+      );
+      return newState;
+    case 'MOVE_ORDER_NEXT':
+      currentPosition = newState[action.payload - 1].position;
+      newPosition = position[currentPosition].next;
+      if (newPosition) newState[action.payload - 1].position = newPosition;
+
+      return newState;
+    case 'MOVE_ORDER_BACK':
+      currentPosition = newState[action.payload - 1].position;
+      newPosition = position[currentPosition].prev;
+      if (newPosition)
+        newState[action.payload - 1].position = position[currentPosition].prev;
+
+      return newState;
+    case 'ADD_INGREDIENT':
+      const { from, ingredient } = action.payload;
+
+      newState.forEach(order => {
+        if (order.position === from) order.ingredients.push(ingredient);
+      });
+      return newState;
     default:
       return state;
   }
+};
+
+const position = {
+  clients: { next: 'conveyor_1', prev: null },
+  conveyor_1: { next: 'conveyor_2', prev: null },
+  conveyor_2: { next: 'conveyor_3', prev: 'conveyor_1' },
+  conveyor_3: { next: 'conveyor_4', prev: 'conveyor_2' },
+  conveyor_4: { next: null, prev: 'conveyor_3' }
 };
 
 export const getOrdersFor = (state, position) =>
